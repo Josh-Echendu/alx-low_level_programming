6ac8e151fef954a43994f7cb7f2a955236effb8d
Josh-Echendu <echendujosh@gmail.com>
2022-04-07 15:52:25 -0700

    josh love

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,17 +1,17 @@
-#include <studio.h>
+#include <stdio.h>
 
 /**
  * main - a program that prints a text
  *
- *  Return:always 0 (success)
- *  
+ * Return: always 0 (success)
+ *
  */
 
 int main(void)
-{
-	
-	puts("\"with proper grammar, but the outcome is a piece of art");
-	
-	return (0);
 
-}	
+{
+	 puts("\"with proper grammar, but the outcome is a piece of art,");
+
+			return (0);
+
+}
