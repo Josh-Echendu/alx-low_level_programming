404f94928b9335387efb0d35e3f7ee6e923a6352
Josh-Echendu <echendujosh@gmail.com>
2022-04-12 04:02:28 -0700

    josh

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+int void print_alphabet(void);
+{
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		_putchar(c);
+		c++;
+	}
+	_putchar ('\n');
+	return (0);
+}
+
