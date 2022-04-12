82b92496785268c8bae19b2e7e0dd28a528a6c5d
Josh-Echendu <echendujosh@gmail.com>
2022-04-12 07:13:24 -0700

    josh

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * void print_alphabet_x10(void) - Entry point
+ * Return: Always 0 (success)
+ */
+void print_alphabet_x10(void);
+{
+	char (c);
+	int i = 0;
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar (c);
+			c++
+		}
+		_putchar ("\n");
+		i++;
+	}
+}
+
