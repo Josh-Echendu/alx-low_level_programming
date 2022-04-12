033c0570ac65cbb63440e032212cf410877b619a
Josh-Echendu <echendujosh@gmail.com>
2022-04-12 04:39:39 -0700

    josh

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,5 @@
-#include  "main.h"
+#include "main.h"
+#include <stdio.h>
 /**
  * main - Entry point
  * Return: Always 0 (success)
@@ -6,7 +7,6 @@
 int main(void)
 {
 	printf("_putchar\n");
-	_putchar ('\n');
 
 	return (0);
 
