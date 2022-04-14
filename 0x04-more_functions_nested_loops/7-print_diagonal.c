e472236972ca150c73c9a8e0c1d520fb75891868
Josh-Echendu <echendujosh@gmail.com>
2022-04-14 08:44:14 -0700

    5-more _numbers

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print a diagonal line
+ * @n: include the parameter
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int count, space;
+
+		for (count = 0; count < n; count++)
+		{
+			for (space = 0; space < n; space++)
+			{
+				if (space == count)
+				{
+					_putchar('\\');
+				}
+				else if (space < count)
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
