77887162be47a8bd7bf842a8bd4163fbc612c4ac
Josh-Echendu <echendujosh@gmail.com>
2022-04-14 05:47:57 -0700

    josh

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* mul - multiply two numbers and print it
+* @a: first parameter
+* @b: second parameter
+* Return: Return 1 when its true
+*/
+int mul(int a, int b)
+{
+	int a = 4, int b = 5 prod;
+
+	prod = a * b
+		printf("the product is %d", prod);
+}
