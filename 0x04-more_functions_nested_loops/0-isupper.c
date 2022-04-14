3ea3fa7bd4bfa3877282e052715fa1a374644c18
Josh-Echendu <echendujosh@gmail.com>
2022-04-14 04:50:24 -0700

    josh

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,11 +1,12 @@
 #include "main.h"
+
 /**
  * _isupper(int c) - Print all the upper case alphabets
  * @c: parameter
  *
  *  Return:  Answer is 1 in success and 0 if it fails
  */
-int _isupper(int c);
+int _isupper(int c)
 {
 	return (c >= 'A' && c <= 'Z');
 }
