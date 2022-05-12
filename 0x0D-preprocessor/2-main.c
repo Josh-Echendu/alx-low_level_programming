4d7d67a9ed2650908a41694de51c4bf36393e2da
Josh-Echendu <echendujosh@gmail.com>
2022-05-12 09:16:53 -0700

    4-sum.h

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * * main - print the name of the
+ * compilation file
+ *
+ * Return: 0 for success
+ */
+
+int main(void)
+
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
